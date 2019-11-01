256123f95a9fe4ee6dd55134c21e2c9fe7d6fa94
ethanbhuang <ethanbhuang@gmail.com>
2019-11-01 14:37:20 -0400

    Updated main ONLY

--- main.py
+++ main.py
@@ -1,35 +1,12 @@
 #import your controller
-import pygame, pyganim,character,spriteSheet,game
+import pygame, pyganim, character, spriteSheet, gameScreen, random
 
-pygame.init()
 
 def main():
-    player = character.Character()
-    gameScreen = game.Game(player,"assets/temp_sprite.png")
-    endJump = pygame.USEREVENT + 1
-    running = True
+    pygame.init()
 
-    while running:
-        #Create an instance on your controller object
-        if pygame.event.get(endJump):
-            player.jump_state = False
-            pygame.time.set_timer(25,0)
-            player.y += 20
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_w:
-                    player.y -= 64
-                elif event.key == pygame.K_s:
-                    player.y += 64
-                elif event.key == pygame.K_SPACE:
-                    player.jump()
-
-        gameScreen.update()
-        gameScreen.render()
-    pygame.quit()
+    game = Controller()
+    game.mainLoop()
 
 if __name__ == "__main__":
     main()
