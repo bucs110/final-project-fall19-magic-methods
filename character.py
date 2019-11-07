5e6e96f48badb02696a1b3f73287251ddcd99d7b
ethanbhuang <ethanbhuang@gmail.com>
2019-11-07 15:50:06 -0500

    Added to Character class

--- character.py
+++ character.py
@@ -1,1 +1,16 @@
 import pygame
+import animatedSprite
+
+# for an animated sprite
+# (self, img_file, x, y,
+# width, height, n_frames, frame_length)
+class Character(pygame.sprite.Sprite):
+    def __init__(self):
+        self.row = 2
+        self.lives = 3
+        self.jumping = False
+        anims = [
+            aniamtedSprite.AnimatedSprite("assets/temp_sprite.png",0,64,
+                            64,64,6,200),
+                            
+        ]
